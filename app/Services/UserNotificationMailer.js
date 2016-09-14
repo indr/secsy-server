4298433353e2df94209204ca1858e9d5a766bd17
indr <mail@indr.ch>
2016-09-14 07:03:24 +0700

    Add reset-password email

--- app/Services/UserNotificationMailer.js
+++ app/Services/UserNotificationMailer.js
@@ -23,3 +23,22 @@ UserNotificationMailer.sendAccountActivation = function * (user, emailToken) {
     message.subject('Confirm your new account')
   })
 }
+
+UserNotificationMailer.sendResetPassword = function * (user, emailToken) {
+  if (!user || typeof (user.toJSON) !== 'function') {
+    throw new Error('Mailer expects a valid instance of User Model')
+  }
+  if (!emailToken || emailToken.length !== 36) {
+    throw new Error('Mailer expects a valid email token')
+  }
+
+  const model = {
+    base_url: Env.get('BASE_URL'),
+    email_token: emailToken
+  }
+  return yield Mail.send([ null, 'emails/user_notifications/reset-password' ], model, function (message) {
+    message.to(user.email)
+    message.from(Env.get('MAIL_FROM_EMAIL'), Env.get('MAIL_FROM_NAME'))
+    message.subject('Reset password')
+  })
+}
