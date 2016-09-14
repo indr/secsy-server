c7167c160ea0be3844a22dce49dc35d3862ec7b4
indr <mail@indr.ch>
2016-09-14 18:45:25 +0700

    Add validator password and token

--- app/Services/Validator.js
+++ app/Services/Validator.js
@@ -1,14 +1,71 @@
 'use strict'
 
+/**
+ * http://indicative.adonisjs.com/#indicative-extending-extending-schema-validator
+ * https://github.com/poppinss/indicative/blob/develop/src/Validations/index.js
+ */
+
 const AdonisValidator = use('Validator')
 const Exceptions = use('App/Exceptions')
-const Validator = exports = module.exports = {}
+
+const Raw = AdonisValidator.is
+
+const skippable = function (value) {
+  return !Raw.existy(value) && value !== null
+}
+
+AdonisValidator.is.extend('password', function (data) {
+  // http://indicative.adonisjs.com/#indicative-extending-extending-raw-validator
+
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*\(\)_])(?=.{8,})/
+  return passwordRegex.test(data)
+})
+
+AdonisValidator.extend('password', function (data, field, message, args, get) {
+  return new Promise((resolve, reject) => {
+    const fieldValue = get(data, field)
+    if (skippable(fieldValue)) {
+      resolve('validation skipped')
+      return
+    }
+    if (Raw.password(fieldValue)) {
+      resolve('validation passed')
+      return
+    }
+    reject(message)
+  })
+})
+
+AdonisValidator.is.extend('token', function (data) {
+  const tokenRegex = /^[0-9a-f]{8}\-[0-9a-f]{4}\-[0-9a-f]{4}\-[0-9a-f]{4}\-[0-9a-f]{12}$/i
+  return tokenRegex.test(data)
+})
+
+AdonisValidator.extend('token', function (data, field, message, args, get) {
+  return new Promise((resolve, reject) => {
+    const fieldValue = get(data, field)
+    if (skippable(fieldValue)) {
+      resolve('validation skipped')
+      return
+    }
+    if (Raw.token(fieldValue)) {
+      resolve('validation passed')
+      return
+    }
+    reject(message)
+  })
+})
+
+const Validator = exports = module.exports = {
+  is: Raw
+}
 
 Validator.validate = function * (data, rules) {
   const validation = yield AdonisValidator.validate(data, rules)
   if (validation.fails()) {
     throw Exceptions.ValidationException.failed(validation.messages())
   }
+  return true
 }
 
 Validator.validateAll = function * (data, rules) {
---
+++ test/integration/Services/Validator.test.js
@@ -0,0 +1,131 @@
+'use strict'
+
+/* eslint-env mocha */
+
+const assert = require('chai').assert
+const setup = require('./../setup')
+const uuid = require('node-uuid')
+require('co-mocha')
+
+describe('Unit | Service | Validator', function () {
+  let sut
+
+  before(function * () {
+    yield setup.loadProviders()
+    yield setup.start()
+
+    sut = make('App/Services/Validator')
+  })
+
+  describe('Validator password', function () {
+    const validPasswords = [
+      'abcDEF123!',
+      'abcDEF123@',
+      'abcDEF123#',
+      'abcDEF123$',
+      'abcDEF123%',
+      'abcDEF123^',
+      'abcDEF123&',
+      'abcDEF123*',
+      'abcDEF123(',
+      'abcDEF123)',
+      'abcDEF123_'
+    ]
+    const invalidPasswords = [
+      'tooshort',
+      'UPPERCASEONLY',
+      'lowercaseonly',
+      '1234567890',
+      '!@#$%^&*_'
+    ]
+
+    describe('Raw validator', function () {
+      it('should pass for valid passwords', function * () {
+        for (let each of validPasswords) {
+          assert.isTrue(sut.is.password(each), each)
+        }
+      })
+
+      it('should fail for invalid passwords', function * () {
+        for (let each of invalidPasswords) {
+          assert.isFalse(sut.is.password(each), each)
+        }
+      })
+    })
+
+    describe('Schema validator', function () {
+      it('should pass for valid passwords', function * () {
+        for (let each of validPasswords) {
+          let validation = yield sut.validate({ field: each }, { field: 'password' })
+          assert.isTrue(validation)
+        }
+      })
+
+      it('should fail for invalid passwords', function * () {
+        for (let each of invalidPasswords) {
+          try {
+            yield sut.validate({ field: each }, { field: 'password' })
+            assert(false)
+          } catch (error) {
+            assert.equal(error.name, 'ValidationException', each)
+            assert.deepEqual(error.fields, [ {
+              field: 'field', message: 'password validation failed on field', validation: 'password'
+            } ])
+          }
+        }
+      })
+    })
+  })
+
+  describe('Validator token', function () {
+    const validTokens = [
+      uuid.v1(),
+      uuid.v4(),
+      uuid.v4().toUpperCase(),
+      uuid.v4().toLowerCase()
+    ]
+    const invalidTokens = [
+      uuid.v4().substr(0, 35),
+      uuid.v4() + 'a',
+      uuid.v4().replace('-', ''),
+      '{' + uuid.v4() + '}'
+    ]
+
+    describe('Raw validator', function () {
+      it('should pass for valid tokens', function * () {
+        for (let each of validTokens) {
+          assert.isTrue(sut.is.token(each), each)
+        }
+      })
+
+      it('should fail for invalid tokens', function * () {
+        for (let each of invalidTokens) {
+          assert.isFalse(sut.is.token(each), each)
+        }
+      })
+    })
+
+    describe('Schema validator', function () {
+      it('should pass for valid tokens', function * () {
+        for (let each of validTokens) {
+          let validation = yield sut.validate({ field: each }, { field: 'token' })
+          assert.isTrue(validation)
+        }
+      })
+
+      it('should fail for invalid tokens', function * () {
+        for (let each of invalidTokens) {
+          try {
+            yield sut.validate({ field: each }, { field: 'token' })
+            assert(false)
+          } catch (error) {
+            assert.equal(error.name, 'ValidationException', each)
+            assert.deepEqual(error.fields, [ {
+              field: 'field', message: 'token validation failed on field', validation: 'token'
+            } ])
+          }
+        }
+      })
+    })
+  })
+})
