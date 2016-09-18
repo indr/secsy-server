63a76bbf3c404ce3bebb22cae4340c6277b7a4e3
indr <mail@indr.ch>
2016-09-18 12:06:29 +0700

    Add debug http delay

--- app/Http/Middleware/Logger.js
+++ app/Http/Middleware/Logger.js
@@ -1,9 +1,17 @@
 'use strict'
 
+const Env = use('Env')
+
 class Logger {
 
   * handle (request, response, next) {
     console.log(`${request.method()} ${request.url()}`)
+
+    if (Env.get('HTTP_DELAY')) {
+      yield new Promise((resolve) => {
+        setTimeout(resolve, Env.get('HTTP_DELAY'))
+      })
+    }
 
     yield next
   }
