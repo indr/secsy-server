46d254a76cbfba58bedbae7671ac9d214dc0395a
indr <mail@indr.ch>
2016-09-24 14:38:17 +0700

    Log status code and duration to console

--- app/Http/Middleware/Logger.js
+++ app/Http/Middleware/Logger.js
@@ -1,11 +1,22 @@
 'use strict'
 
+const colors = require('colors/safe')
 const Env = use('Env')
+const printf = require('printf')
+
+const colorStatus = function (status) {
+  let colorFn = status >= 500 ? colors.red // red
+    : status >= 400 ? colors.yellow // yellow
+    : status >= 300 ? colors.cyan // cyan
+    : status >= 200 ? colors.green // green
+    : null // no color
+
+  return colorFn ? colorFn(status) : status
+}
 
 class Logger {
-
   * handle (request, response, next) {
-    console.log(`${request.method()} ${request.url()}`)
+    let t = process.hrtime()
 
     if (Env.get('HTTP_DELAY')) {
       yield new Promise((resolve) => {
@@ -14,8 +25,12 @@ class Logger {
     }
 
     yield next
-  }
 
+    t = process.hrtime(t)
+    const status = colorStatus(response.response.statusCode)
+    const ms = printf('%.3f ms', (t[ 0 ] * 1000 + t[ 1 ] / 1000) / 1000)
+    console.log(`${request.method()} ${request.url()} ${status} ${ms}`)
+  }
 }
 
 module.exports = Logger
